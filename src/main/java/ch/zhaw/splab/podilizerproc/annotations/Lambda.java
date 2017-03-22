b406cca068fc5cb791c6f0dba7d10f6e813a33ef
dord <dord@zhaw.ch>
2017-03-22 16:40:48 +0100

    annotation processor service added; annotatted methods visitor implemented; pom.xml updated

--- src/main/java/ch/zhaw/splab/podilizerproc/annotations/Lambda.java
+++ src/main/java/ch/zhaw/splab/podilizerproc/annotations/Lambda.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark the method you want to deploy to FaaS service
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Lambda {
--- src/main/java/ch/zhaw/splab/podilizerproc/annotations/LambdaProcessor.java
+++ src/main/java/ch/zhaw/splab/podilizerproc/annotations/LambdaProcessor.java
@@ -1,12 +1,69 @@
 package ch.zhaw.splab.podilizerproc.annotations;
 
-import javax.annotation.processing.*;
-import javax.lang.model.element.TypeElement;
-import java.util.Set;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.util.TreePath;
+import com.sun.source.util.TreePathScanner;
+import com.sun.source.util.Trees;
 
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.*;
+
+/**
+ * Processor of {@link Lambda} annotation
+ */
 @SupportedAnnotationTypes({"ch.zhaw.splab.podilizerproc.annotations.Lambda"})
 public class LambdaProcessor extends AbstractProcessor {
+    private Trees trees;
+
+    /**
+     * Initialization of {@link ProcessEnvironment} object and {@ling Trees} object
+     * @param processingEnv
+     */
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        trees = Trees.instance(processingEnv);
+    }
+
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        Set<? extends Element> annotatedMethods = roundEnv.getElementsAnnotatedWith(Lambda.class);
+        Messager messager = processingEnv.getMessager();
+        MethodScanner visitor = new MethodScanner();
+        messager.printMessage(Diagnostic.Kind.NOTE, "Lambda annotations: " + annotatedMethods.size());
+        for (Element element :
+                annotatedMethods) {
+            messager.printMessage(Diagnostic.Kind.NOTE, element.getSimpleName());
+            TreePath tp = trees.getPath(element);
+            visitor.scan(tp, trees);
+        }
+        messager.printMessage(Diagnostic.Kind.NOTE, "There are methods\n" +
+                Arrays.toString(visitor.getMethods().toArray()));
+        return true;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
+    /**
+     * Method visitor
+     */
+    private class MethodScanner extends TreePathScanner {
+        private List<MethodTree> methods = new ArrayList<>();
+
+        @Override
+        public Object visitMethod(MethodTree methodTree, Object o) {
+            methods.add(methodTree);
+            return null;
+        }
+
+        public List<MethodTree> getMethods() {
+            return methods;
+        }
     }
 }
