6e197c10af3403ea7c6d9431e92aee7ca6072ccc
Sebastian Heunke <sebastian.heunke@hm.edu>
2020-05-19 15:50:57 +0200

    Added some notes for later implementations.

--- src/main/java/ch/zhaw/splab/podilizerproc/annotations/LambdaProcessor.java
+++ src/main/java/ch/zhaw/splab/podilizerproc/annotations/LambdaProcessor.java
@@ -2,9 +2,7 @@ package ch.zhaw.splab.podilizerproc.annotations;
 
 import ch.zhaw.splab.podilizerproc.awslambda.Functions;
 import ch.zhaw.splab.podilizerproc.awslambda.LambdaFunction;
-import com.sun.source.tree.ClassTree;
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.*;
 import com.sun.source.util.SimpleTreeVisitor;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.TreePathScanner;
@@ -14,6 +12,10 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ExecutableType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
@@ -26,6 +28,7 @@ import java.util.*;
 @SupportedAnnotationTypes({"ch.zhaw.splab.podilizerproc.annotations.Lambda"})
 public class LambdaProcessor extends AbstractProcessor {
     private Trees trees;
+    private Types typeUtils;
 
     /**
      * Initialization of {@link ProcessEnvironment} object and {@link Trees} object
@@ -37,11 +40,13 @@ public class LambdaProcessor extends AbstractProcessor {
         super.init(processingEnv);
         trees = Trees.instance(processingEnv);
 
+        System.out.println("[TERMITE] Annotation Proccessor init.");
+        typeUtils = processingEnv.getTypeUtils();
+
     }
 
 
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        ;
         List<LambdaFunction> functions = new ArrayList<>();
 
         Set<? extends Element> annotatedMethods = roundEnv.getElementsAnnotatedWith(Lambda.class);
@@ -54,6 +59,14 @@ public class LambdaProcessor extends AbstractProcessor {
             MethodScanner methodScanner = new MethodScanner();
             TypeScanner typeScanner = new TypeScanner();
             CUVisitor cuVisitor = new CUVisitor();
+
+            //TODO: Find and package non primitive types.
+            // ExecutableType emeth = (ExecutableType)element.asType();
+            // 1. Passed parameters
+            // 2. Return parameters
+            // 3. locally used types. (Hardest)
+            // for (TypeMirror parameterType : emeth.getParameterTypes()) {
+            //    Element element1 = typeUtils.asElement(parameterType);
 
 
             TreePath tp = trees.getPath(element);
